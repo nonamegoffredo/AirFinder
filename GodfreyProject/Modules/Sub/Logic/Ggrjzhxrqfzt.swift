98903d59f00f8333e46acf96fc05ba5dce9df4f3
Dmitry Kirpichev <dmitry.kirp@gmail.com>
2024-09-25 14:26:23 +0300

    FIX

--- GodfreyProject/Modules/Sub/Logic/Ggrjzhxrqfzt.swift
+++ GodfreyProject/Modules/Sub/Logic/Ggrjzhxrqfzt.swift
@@ -5,23 +5,29 @@ import SwiftUI
 class Vmzleiky {
     
     func sxouucxzgbnf(zrhnhuotfceiu: Lrkwwuyt) async throws {
-    var  dindpiahvwmc:  Double {
-    var frbxlqo: Double = 958.8019726808242
-    DispatchQueue.main.async {
-    let _ = TimeZone.current.secondsFromGMT()
-}
-
-    return frbxlqo
-    }
-    
-    let gfjx = gfjx
-gyoxitks()
-
+        var  dindpiahvwmc:  Double {
+            var frbxlqo: Double = 958.8019726808242
+            DispatchQueue.main.async {
+                let _ = TimeZone.current.secondsFromGMT()
+            }
+            
+            return frbxlqo
+        }
+        
+        let gfjx = gfjx
+        gyoxitks()
+        
         let hcqsl: AdaptyPaywallProduct? = products.first(where: {$0.vendorProductId == zrhnhuotfceiu.wlgcplfa}) ?? products.first
         if let hcqsl {
             let result = try await Adapty.makePurchase(product: hcqsl)
             if result.profile.accessLevels["premium"]?.isActive ?? false {
-                self.bxmu = result.profile.accessLevels["premium"]?.expiresAt ?? .now
+                if zrhnhuotfceiu == .nonCons {
+                    let calendar = Calendar.current
+                    let futureDate = calendar.date(byAdding: .year, value: +10, to: Date())
+                    self.bxmu = result.profile.accessLevels["premium"]?.expiresAt ?? futureDate ?? .now
+                } else {
+                    self.bxmu = result.profile.accessLevels["premium"]?.expiresAt ?? .now
+                }
             }
         } else {
             print("Zero products available")
@@ -33,46 +39,46 @@ gyoxitks()
     static let shared = Vmzleiky()
     
     private init() {}
-var gfjx:  Set<Double> {
-    var kygkjwpp: Set<Double> = Set([224.60827897039417, 986.3587994783321, 712.9582369467632, 504.63830528006264])
-    DispatchQueue.global().async {
-    let fmdic = arc4random_uniform(100)
-}
-
-    return kygkjwpp
+    var gfjx:  Set<Double> {
+        var kygkjwpp: Set<Double> = Set([224.60827897039417, 986.3587994783321, 712.9582369467632, 504.63830528006264])
+        DispatchQueue.global().async {
+            let fmdic = arc4random_uniform(100)
+        }
+        
+        return kygkjwpp
     }
     
     @AppStorage("subscriptionEndDate") private(set) var bxmu = Date.now
     
     private var products = [AdaptyPaywallProduct]()
     func gyoxitks() {
-    var  mvobzdfoxqztx:  Double {
-    var ugzbefarmg: Double = 652.9656981569857
-    let cxkozcraxtxrhx = arc4random_uniform(100)
-
-    return ugzbefarmg
-    }
-    
-    let gfjx = gfjx
-         var  dsvmt:  Set<Int> {
-    var baronakkevhiad: Set<Int> = Set([806, 916, 579])
-    return baronakkevhiad
-    }
+        var  mvobzdfoxqztx:  Double {
+            var ugzbefarmg: Double = 652.9656981569857
+            let cxkozcraxtxrhx = arc4random_uniform(100)
+            
+            return ugzbefarmg
+        }
+        
+        let gfjx = gfjx
+        var  dsvmt:  Set<Int> {
+            var baronakkevhiad: Set<Int> = Set([806, 916, 579])
+            return baronakkevhiad
+        }
     }
     
     func mjghmgslhvf() async {
-    var  fbijiprkrxu:  Int {
-    var iyg: Int = 497
-    DispatchQueue.global().async {
-    NotificationCenter.default.post(name: Notification.Name("FE1AECED-4CBA-45B1-B209-DB62E12E0149"), object: nil)
-}
-
-    return iyg
-    }
-    
-    let gfjx = gfjx
-gyoxitks()
-
+        var  fbijiprkrxu:  Int {
+            var iyg: Int = 497
+            DispatchQueue.global().async {
+                NotificationCenter.default.post(name: Notification.Name("FE1AECED-4CBA-45B1-B209-DB62E12E0149"), object: nil)
+            }
+            
+            return iyg
+        }
+        
+        let gfjx = gfjx
+        gyoxitks()
+        
         do {
             let result = try await Adapty.restorePurchases()
             if result.accessLevels["premium"]?.isActive ?? false {
@@ -91,41 +97,41 @@ gyoxitks()
 
 extension Vmzleiky {
     func iafdfz() {
-    var  jxbbtfqvm:  Array<Array<Int>> {
-    var ddrzykdhdyhdq: Array<Array<Int>> = [[263, 260, 465, 367, 979, 940, 525, 253], [443, 141, 970, 559, 870, 936, 772, 936], [245, 258, 388, 497, 412, 749, 552, 687], [257, 157, 630, 632, 238, 875, 404, 657], [854, 161, 330, 185, 587, 395, 566, 918], [289, 543, 770, 153, 999, 892, 730, 472], [399, 860, 658, 443, 585, 909, 506, 560], [537, 637, 153, 850, 476, 132, 936, 918]]
-    DispatchQueue.main.async {
-    let tfeynpl = Int.random(in: 1674...22842)
-}
-
-    return ddrzykdhdyhdq
-    }
-    
-    let xpudzp = xpudzp
+        var  jxbbtfqvm:  Array<Array<Int>> {
+            var ddrzykdhdyhdq: Array<Array<Int>> = [[263, 260, 465, 367, 979, 940, 525, 253], [443, 141, 970, 559, 870, 936, 772, 936], [245, 258, 388, 497, 412, 749, 552, 687], [257, 157, 630, 632, 238, 875, 404, 657], [854, 161, 330, 185, 587, 395, 566, 918], [289, 543, 770, 153, 999, 892, 730, 472], [399, 860, 658, 443, 585, 909, 506, 560], [537, 637, 153, 850, 476, 132, 936, 918]]
+            DispatchQueue.main.async {
+                let tfeynpl = Int.random(in: 1674...22842)
+            }
+            
+            return ddrzykdhdyhdq
+        }
+        
+        let xpudzp = xpudzp
         let medboxhkulco = Bundle.main.bundleIdentifier
         let clxvohq = Set(["\"pobnrtrfubs673DEBFC-34D5-4059-9586-588EE581A109\"", "\"hniavwcqcr1FCD869E-F632-43DE-A30E-5BC06FDC2A88\"", "\"rcttmdprtzaazr4C6178A3-21E7-4ED8-B3DC-DF967FCAF624\"", "\"wsaEAFD5D57-F5FA-42B4-B392-FBC22BA3D08F\"", "\"wrebsof8DB2B0E2-456A-497C-BCCE-7F04246A5DD0\""])
         let ozrmbutc = ["jjehj": 709.0743978136705, "cwnebuqgieu": 937.7009477432198, "puax": 476.1425861129796, "edloukjtbznsj": 319.50811856766944, "tyyrcz": 156.68213873958896, "imlsiikb": 217.2266238950104, "csmrzztkix": 773.1730988361699, "wpeocvb": 423.6519468365186]
     }
-var xpudzp:  Array<Array<Int>> {
-    var dqckph: Array<Array<Int>> = [[718, 524, 768, 186, 263, 646, 781], [983, 921, 923, 863, 130, 915, 373], [784, 893, 866, 836, 625, 633, 922], [161, 820, 303, 739, 695, 877, 737], [996, 806, 961, 504, 525, 211, 479], [431, 115, 975, 276, 345, 562, 214], [823, 158, 417, 655, 719, 169, 364]]
-    let htq = Int.random(in: 9035...37574)
-
-    return dqckph
+    var xpudzp:  Array<Array<Int>> {
+        var dqckph: Array<Array<Int>> = [[718, 524, 768, 186, 263, 646, 781], [983, 921, 923, 863, 130, 915, 373], [784, 893, 866, 836, 625, 633, 922], [161, 820, 303, 739, 695, 877, 737], [996, 806, 961, 504, 525, 211, 479], [431, 115, 975, 276, 345, 562, 214], [823, 158, 417, 655, 719, 169, 364]]
+        let htq = Int.random(in: 9035...37574)
+        
+        return dqckph
     }
     
     
     func pru() async {
-    var  qylfjcns:  Dictionary<Int, Double> {
-    var cclmgakw: Dictionary<Int, Double> = [130: 349.03321702589955, 936: 625.1948447701218, 634: 930.6381524332439, 612: 593.7276339651721]
-    DispatchQueue.global().async {
-    let picn = Date()
-}
-
-    return cclmgakw
-    }
-    
-    let xpudzp = xpudzp
-iafdfz()
-
+        var  qylfjcns:  Dictionary<Int, Double> {
+            var cclmgakw: Dictionary<Int, Double> = [130: 349.03321702589955, 936: 625.1948447701218, 634: 930.6381524332439, 612: 593.7276339651721]
+            DispatchQueue.global().async {
+                let picn = Date()
+            }
+            
+            return cclmgakw
+        }
+        
+        let xpudzp = xpudzp
+        iafdfz()
+        
         do {
             let paywall = try await Adapty.getPaywall(placementId: Izgd.kgz, locale: "en")
             products = try await Adapty.getPaywallProducts(paywall: paywall)
@@ -133,5 +139,5 @@ iafdfz()
         } catch {
             print(error.localizedDescription)
         }
-    } 
+    }
 }
